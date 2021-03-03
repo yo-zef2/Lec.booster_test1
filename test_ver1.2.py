b08171463546ebd54e1f241e4164d7596662852f
Arii Keisuke <ariikeisuke@gmail.com>
2021-03-03 18:45:42 +0900

    r2

--- test_ver1.2.py
+++ test_ver1.2.py
@@ -52,8 +52,8 @@ if st.button("▼お住まいの市区町村の音楽教室情報を知る▼"):
 
     # ファイルを読み込む
 
-    music_school_df = pd.read_excel("/Users/ariikeisuke/Desktop/Datamix_ver1.2/市町村別人口/pivot_data_music_school.xlsx")
-    populations = pd.read_excel("/Users/ariikeisuke/Desktop/Datamix_ver1.2/市町村別人口/市区町村別人口.xlsx")
+    music_school_df = pd.read_excel("./pivot_data_music_school.xlsx")
+    populations = pd.read_excel("./市区町村別人口.xlsx")
 
     # 市区町村平均人口の算出
 
